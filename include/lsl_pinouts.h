47164d955ae57bf0ddd3cc7e6f8e3bf4b141e156
Plunne <plunne.celeste@gmail.com>
2023-03-24 21:34:20 +0100

    Patch : stdint + ADC v1

--- include/lsl_pinouts.h
+++ include/lsl_pinouts.h
@@ -9,19 +9,19 @@
 
 typedef struct {
     GPIO_TypeDef *PORTx;
-    unsigned char pin;
-    unsigned char mode;
+    uint8_t pin;
+    uint8_t mode;
 }LSL_Pinout;
 
 /* Enable */
 void LSL_PINOUTS_Enable(GPIO_TypeDef *PORTx);
 
 /* Mode */
-void LSL_PINOUTS_SetMode(GPIO_TypeDef *PORTx, unsigned char pin, unsigned char mode);
-void LSL_PINOUTS_ClearMode(GPIO_TypeDef *PORTx, unsigned char pin);
+void LSL_PINOUTS_SetMode(GPIO_TypeDef *PORTx, uint8_t pin, uint8_t mode);
+void LSL_PINOUTS_ClearMode(GPIO_TypeDef *PORTx, uint8_t pin);
 
 /* Pinout */
-void LSL_PINOUTS_SetPinout(GPIO_TypeDef *PORTx, unsigned char pin, unsigned char mode);
+void LSL_PINOUTS_SetPinout(GPIO_TypeDef *PORTx, uint8_t pin, uint8_t mode);
 void LSL_PINOUTS_InitPinout(LSL_Pinout *pinout);
 
 #endif // __LSL_PINOUTS_H
--- src/main.c
+++ src/main.c
@@ -10,25 +10,13 @@ int main(void) {
 	LSL_Init_Registers();
 
 	/* Init ADC7/1 */
-	// Enable ADC1
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+	LSL_ANALOG_ADC_Start(ADC1, 2);
 
-	// Set ADC Frequency (max allowed 14Mhz)
-	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV6;
+	/* Select channel sequence */
+	LSL_ANALOG_ADC_SetSequence(ADC1, 2, POTAR.pin);
 
-	// Run ADC1 (ADON)
-	ADC1->CR2 |= ADC_CR2_ADON;
-
-	// Set number of conversions for ADC1
-	ADC1->SQR1 &= ~(0xF << 20);	// Clear number of conversions
-	ADC1->SQR1 |= (0x1 << 20);	// Set number of conversions (1 in this case)
-
-	// Set conversion order for ADC1
-	ADC1->SQR3 |= (7 << (5 * 0)); // Select ADC7 for the first conversion (ADC1_SQR3_SQ1 = 7)
-
-	// Calibrate ADC1
-	ADC1->CR2 |= ADC_CR2_CAL; // Calibrate ADC
-	while(ADC1->CR2 & ADC_CR2_CAL); // Waiting for calibration
+	/* Calibrate ADC */
+	LSL_ANALOG_ADC_Calibrate(ADC1);
 
 	/* Init Variables */
 	uint16_t adc_value = 0;
@@ -36,14 +24,9 @@ int main(void) {
 	/* Super Loop */
 	while (1) {
 
-		// ADC1 Capture
-		ADC1->CR2 |= ADC_CR2_ADON;			// Launch ADC capture
-		while(!(ADC1->SR & ADC_SR_EOC));	// Waiting for capture
-		ADC1->SR &= ~ADC_SR_EOC;			// Reset end of conversion flag
-
-		// ADC1 Data
-		adc_value = (uint16_t)(ADC1->DR & ~(0xF << 12)); // Get ADC Data without MSB Bits 12 -> 15 (because it's a 12bits ADC not 16)
-
+		LSL_ANALOG_ADC_Read(ADC1); // Get blank ADC Data because we have set an empty ADC input as first sequence (increment to next ADC conv)
+		adc_value = LSL_ANALOG_ADC_Read(ADC1); // Get ADC Data without MSB Bits 12 -> 15 (because it's a 12bits ADC not 16)
+		
 		// Delay
 		LSL_DIGITAL_Write(&LED, TOGGLE);
 		LSL_UTILS_DelayMs(adc_value); 					// Delay (Be careful sw_cpt value depends of this delay)
